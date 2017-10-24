0264c5a4abbfc8d1ad066d2868c1f468ac1f3b66
Bryce Young <bkyoung@live.com>
2017-10-24 13:58:57 -0400

    initial file structure and test case creation

---
+++ src/TidyNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class TidyNumbers {
+	
+private static int n; //number to find tidy number inside of
+private static int testCases;//number of test cases
+private static boolean tidy; //true or false if the number is tidy
+private static Scanner sc; 
+
+	public static void main(String[] args) {
+		sc = new Scanner("B-small-practice.in");
+
+	}
+
+}
